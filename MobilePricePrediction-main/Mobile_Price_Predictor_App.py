bee3f2ac5f0094006d649f0e471d5ea8c5c8a5de
yusmitha <yusmithalekha@gmail.com>
2026-06-23 20:38:30 -0500

    Fix pickle file paths for deployment

--- MobilePricePrediction-main/Mobile_Price_Predictor_App.py
+++ MobilePricePrediction-main/Mobile_Price_Predictor_App.py
@@ -1,49 +1,52 @@
 import streamlit as st
 import pickle
 import numpy as np
-
-# import the model
-pipe = pickle.load(open('pipe.pkl','rb'))
-df = pickle.load(open('df.pkl','rb'))
-
+import os
+ 
+# Fix file paths for deployment
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+pipe = pickle.load(open(os.path.join(BASE_DIR, 'pipe.pkl'), 'rb'))
+df = pickle.load(open(os.path.join(BASE_DIR, 'df.pkl'), 'rb'))
+ 
 st.title("Mobile Price Predictor")
-
+ 
 # brand
-Mobile_Brand = st.selectbox('Brand',df['Brand'].unique())
-
+Mobile_Brand = st.selectbox('Brand', df['Brand'].unique())
+ 
 # Ram
-Ram = st.selectbox('RAM(in GB)',[1,2,3,4,6,8,12,16])
-
+Ram = st.selectbox('RAM(in GB)', [1, 2, 3, 4, 6, 8, 12, 16])
+ 
 # Storage
-Storage = st.selectbox('Storage(in GB)',[8,16,32,64,128,256,512,1024])
-
+Storage = st.selectbox('Storage(in GB)', [8, 16, 32, 64, 128, 256, 512, 1024])
+ 
 # BackCameras
-Number_Of_Cameras = st.selectbox('Number of Back Cameras',[1,2,3,4])
-
+Number_Of_Cameras = st.selectbox('Number of Back Cameras', [1, 2, 3, 4])
+ 
 # Primary Camera Pixels
-Primary_Camera = st.selectbox('Primary Camera(in MP)',[5,8,12,13,16,48,50,64,100,108,200])
-
+Primary_Camera = st.selectbox('Primary Camera(in MP)', [5, 8, 12, 13, 16, 48, 50, 64, 100, 108, 200])
+ 
 # Display Type
-Display_Type = st.selectbox('Display Type',df['Display Type'].unique())
-
+Display_Type = st.selectbox('Display Type', df['Display Type'].unique())
+ 
 # screen size
-screen_size = st.selectbox('Screen Size(in inches)',df['Screen Size(in)'].unique())
-
+screen_size = st.selectbox('Screen Size(in inches)', df['Screen Size(in)'].unique())
+ 
 # Android
-Android = st.selectbox('Android Version(in V)',df['Android Version'].unique())
-
+Android = st.selectbox('Android Version(in V)', df['Android Version'].unique())
+ 
 # Battery
-Battery = st.selectbox('Battery(in mAh)',df['Battery(mAh)'].unique())
-
+Battery = st.selectbox('Battery(in mAh)', df['Battery(mAh)'].unique())
+ 
 # Number Of Cores
-Cores = st.selectbox('Number of Cores',df['Number of Cores'].unique())
-
+Cores = st.selectbox('Number of Cores', df['Number of Cores'].unique())
+ 
 # Processor
-Processor = st.selectbox('Processor',df['Processor Brand'].unique())
-
-
+Processor = st.selectbox('Processor', df['Processor Brand'].unique())
+ 
+ 
 if st.button('Predict Price'):
     query = np.array([Mobile_Brand, Ram, Storage, Number_Of_Cameras, Primary_Camera,
                       Display_Type, screen_size, Android, Battery, Cores, Processor])
     query = query.reshape(1, 11)
-    st.title(str(int(np.exp(pipe.predict(query)[0]))))
+    predicted_price = int(np.exp(pipe.predict(query)[0]))
+    st.success(f"💰 Estimated Price: ₹{predicted_price:,}")
